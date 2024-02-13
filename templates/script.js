0679eb7e65777e1b3f811a02f6e84c2c47676171
cryoleet <vishalpremnath09@gmail.com>
2024-02-14 00:15:12 +0530

    almost done with semester page

--- templates/script.js
+++ templates/script.js
@@ -1,7 +1,33 @@
-let addRowBtn = document.getElementById("addBtn");
-addRowBtn.addEventListener("click", addRow);
+document.getElementById("iatTotalRows").value = document.getElementById("iatTable").rows.length;
+document.getElementById("attendanceTotalRows").value = document.getElementById("attendanceTable").rows.length;
+console.log(document.getElementById("iatTotalRows").value); 
 
-function addRow() {
+let attendanceBtn = document.getElementById("attendanceAddBtn");
+attendanceBtn.addEventListener("click", addRowAttendance);
+
+let iatBtn = document.getElementById("iatAddBtn");
+iatBtn.addEventListener("click", addRowIat);
+
+
+function addRowIat() {
+    let tbody = document.getElementById("iatTable");
+    let row = tbody.insertRow(-1);
+
+    for (let i = 0; i < 8; i++)
+    {
+        c = row.insertCell(i);
+        elem = document.createElement("input");
+        elem.type = "text";
+        c.appendChild(elem);
+
+    }
+
+    document.getElementById("iatTotalRows").value = Number(document.getElementById("iatTotalRows").value) + 1;
+    console.log(document.getElementById("iatTotalRows").value);
+
+}
+
+function addRowAttendance() {
     let tbody = document.getElementById("attendanceTable");
     let row = tbody.insertRow(-1);
 
@@ -13,5 +39,8 @@ function addRow() {
         c.appendChild(elem);
 
     }
+
+    document.getElementById("attendanceTotalRows").value = Number(document.getElementById("attendanceTotalRows").value) + 1;
+    console.log(document.getElementById("attendanceTotalRows").value);
 
 }
