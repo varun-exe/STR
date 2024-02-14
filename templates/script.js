bc2bcdfa9c716752516eb3f240522978a861858d
cryoleet <vishalpremnath09@gmail.com>
2024-02-14 09:55:05 +0530

    condensed JS code for add row buttons

--- templates/script.js
+++ templates/script.js
@@ -1,46 +1,15 @@
-document.getElementById("iatTotalRows").value = document.getElementById("iatTable").rows.length;
-document.getElementById("attendanceTotalRows").value = document.getElementById("attendanceTable").rows.length;
-console.log(document.getElementById("iatTotalRows").value); 
+addButtons = document.querySelectorAll(".addBtn");
 
-let attendanceBtn = document.getElementById("attendanceAddBtn");
-attendanceBtn.addEventListener("click", addRowAttendance);
-
-let iatBtn = document.getElementById("iatAddBtn");
-iatBtn.addEventListener("click", addRowIat);
+addButtons.forEach(function(btn) {
+    document.getElementById(btn.getAttribute("data-increement")).value = 1;
+    btn.addEventListener("click", addRow);
+})
 
 
-function addRowIat() {
-    let tbody = document.getElementById("iatTable");
-    let row = tbody.insertRow(-1);
-
-    for (let i = 0; i < 8; i++)
-    {
-        c = row.insertCell(i);
-        elem = document.createElement("input");
-        elem.type = "text";
-        c.appendChild(elem);
-
-    }
-
-    document.getElementById("iatTotalRows").value = Number(document.getElementById("iatTotalRows").value) + 1;
-    console.log(document.getElementById("iatTotalRows").value);
-
-}
-
-function addRowAttendance() {
-    let tbody = document.getElementById("attendanceTable");
-    let row = tbody.insertRow(-1);
-
-    for (let i = 0; i < 12; i++)
-    {
-        c = row.insertCell(i);
-        elem = document.createElement("input");
-        elem.type = "text";
-        c.appendChild(elem);
-
-    }
-
-    document.getElementById("attendanceTotalRows").value = Number(document.getElementById("attendanceTotalRows").value) + 1;
-    console.log(document.getElementById("attendanceTotalRows").value);
-
+function addRow() {
+    table = document.getElementById(this.getAttribute("data-for"));
+    console.log(table);
+    row = table.rows[0];
+    table.insertAdjacentHTML("beforeend", row.innerHTML)
+    document.getElementById(this.getAttribute("data-increement")).value = Number(document.getElementById(this.getAttribute("data-increement")).value) + 1; 
 }
